476e4706b85332fd50ba598fd5f8267aad7a02ea
Lee <plbrannan95@gmail.com>
2022-03-06 13:21:00 -0500

    Cleanup

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,8 +1,9 @@
 import Head from "next/head";
-import { useState } from "react";
+import React, { useState } from "react";
 import styles from "./layout.module.scss";
 
-export const defaultSiteTitle = "bananabrann - web development and software engineering";
+export const defaultSiteTitle =
+  "bananabrann - web development and software engineering";
 
 export default function Layout({
   children,
@@ -16,22 +17,16 @@ export default function Layout({
   const [siteTitle, setSiteTitle] = useState<string>(title ?? defaultSiteTitle);
 
   return (
-    <div className={styles.container}>
+    <React.Fragment>
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <title>{siteTitle}</title>
         <meta name="description" content="Website of bananabrann." />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
 
-      <main>{children}</main>
-    </div>
+      <main className={`${styles.wrapper}`}>{children}</main>
+    </React.Fragment>
   );
 }
