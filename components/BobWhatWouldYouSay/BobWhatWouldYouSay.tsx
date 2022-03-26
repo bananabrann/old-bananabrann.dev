274ec26f0051306abbee8b9e6bb2deb9b8bf41a4
Lee <plbrannan95@gmail.com>
2022-03-26 12:09:31 -0400

    Escape quote signs

--- components/BobWhatWouldYouSay/BobWhatWouldYouSay.tsx
+++ components/BobWhatWouldYouSay/BobWhatWouldYouSay.tsx
@@ -11,8 +11,8 @@ export default function BobWhatWouldYouSay() {
         */}
         {/* prettier-ignore */}
         <div className={`absolute z-50 text-4xl font-semibold -mt-20 ml-[36vw] md:text-5xl md:ml-[36vw] lg:ml-[26vw] `}>
-          <h3>"What would you say ...</h3>
-          <h3 className="ml-[25%]">you do here?"</h3>
+          <h3>{`"What would you say ...`}</h3>
+          <h3 className="ml-[25%]">{`you do here?"`}</h3>
         </div>
 
         <div className={styles.bobimagewrapper}>
--- pages/index.tsx
+++ pages/index.tsx
@@ -21,11 +21,11 @@ export default function Home() {
             <h1 className="text-7xl font-bold ">Succeed with your software</h1>
             <br />
             <p className="text-3xl">
-              Whether it's a new IT system from scratch, or a touch-up on your
-              website, I'm here to help you succeed online.
+              {`Whether it's a new IT system from scratch, or a touch-up on your
+              website, I'm here to help you succeed online.`}
             </p>
             <br />
-            <p className="text-3xl">When you're ready, email me at </p>
+            <p className="text-3xl">{`When you're ready, email me at `}</p>
             <h3 className="text-4xl font-bold">hi@bananabrann.dev</h3>
           </div>
         </div>
