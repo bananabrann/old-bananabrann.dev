aa5e0ed2e028cf8adea3eae055bd5a2a01813447
Lee <plbrannan95@gmail.com>
2022-04-03 15:17:59 -0400

    Skills list formatting

--- pages/skills.tsx
+++ pages/skills.tsx
@@ -7,6 +7,7 @@ import ContentBox, {
 import Footer from "../components/Footer/Footer";
 import Layout from "../components/Layout/Layout";
 import Navbar from "../components/Navbar/Navbar";
+import { useState } from "react";
 
 type TechList = {
   experienced: Array<string>;
@@ -14,46 +15,11 @@ type TechList = {
   exposure: Array<string>;
 };
 
-export default function Skills({ techList }: { techList: TechList }) {
-  let skillsListContent = techList.experienced.map((item) => {
-    return <p>{item}</p>;
-  });
-
-  return (
-    <Layout title="bananabrann - skills">
-      <Navbar />
-
-      <div className="flex gap-3 m-9">
-        <div>
-          <h1 className="">Skills</h1>
-          <p>
-            I've done a lot of things in my career. I was a U.S. Marine,
-            professional tuba player, and now most recently a software developer
-          </p>
-        </div>
-
-        <div>
-          <ContentBox
-            title={`"OMG! Where's your resume?!"`}
-            symbol={ContentBoxSymbol.InformationSign}
-          >
-            <p>{`I don't display a resume to the public due to privacy concerns. If you want one, reach out to me at hi@bananabrann.dev.`}</p>
-          </ContentBox>
-        </div>
-      </div>
-
-      {skillsListContent}
-
-      <Footer />
-    </Layout>
-  );
-}
-
 export async function getStaticProps() {
   /**
    * Construct what the path looks like.
    */
-  const techListFileName = "tec.yaml";
+  const techListFileName = "tech.yaml";
   const techListFileFullPath = path.resolve("./public", techListFileName);
 
   try {
@@ -106,4 +72,64 @@ export async function getStaticProps() {
       },
     };
   }
+}
+
+export default function Skills({ techList }: { techList: TechList }) {
+  const [isSkillsListExpanded, setIsSkillsListExpanded] =
+    useState<boolean>(false);
+
+  // prettier-ignore
+  let skillsListContent = (
+    <div className="px-8 -indent-4 flex justify-evenly mx-auto flex-col gap-8 sm:flex-row ">
+      <div className="w-72" >
+        <h4 className="mb-2 text-center">Experienced</h4>
+        <ul>
+          {techList.experienced.map((item: string) => <li className="">• {item}</li>)}
+        </ul>
+      </div>
+
+      <div>
+        <h4 className="mb-2 text-center">Proficient</h4>
+        <ul>
+          {techList.proficient.map((item: string) => <li>• {item}</li>)}
+        </ul>
+      </div>
+
+      <div>
+        <h4 className="mb-2 text-center">Exposure</h4>
+        <ul>
+          {techList.exposure.map(item => <li>• {item}</li>)}
+        </ul>
+      </div>
+    </div>
+  );
+
+  return (
+    <Layout title="bananabrann - skills">
+      <Navbar />
+
+      <div className="flex gap-3 m-9">
+        <div>
+          <h1 className="">Skills</h1>
+          <p>
+            I've done a lot of things in my career. I was a U.S. Marine,
+            professional tuba player, and now most recently a software developer
+          </p>
+        </div>
+
+        <div>
+          <ContentBox
+            title={`"OMG! Where's your resume?!"`}
+            symbol={ContentBoxSymbol.InformationSign}
+          >
+            <p>{`I don't display a resume to the public due to privacy concerns. If you want one, reach out to me at hi@bananabrann.dev.`}</p>
+          </ContentBox>
+        </div>
+      </div>
+
+      {skillsListContent}
+
+      <Footer />
+    </Layout>
+  );
 }
