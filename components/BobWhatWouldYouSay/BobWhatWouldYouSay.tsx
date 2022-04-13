9e36fa0284589ac613decd2404b114108ea8a41e
bananabrann <plbrannan95@gmail.com>
2022-04-13 10:26:09 -0400

    Fix Bob picture on mobile (#9)

--- components/BobWhatWouldYouSay/BobWhatWouldYouSay.tsx
+++ components/BobWhatWouldYouSay/BobWhatWouldYouSay.tsx
@@ -1,30 +1,33 @@
-import Image from "next/image";
-import BobTrianglePng from "../../res/png/bob-triangle.png";
+import BobPng from "../../res/png/bob.png";
 import styles from "./BobWhatWouldYouSay.module.scss";
+
+/**
+ * NOTE -- The easiest way to edit these SVGs is in Adobe XD. Make your changes,
+ * then right-click "Copy SVG code", then paste and tidy it up here.
+ * 
+ * NOTE -- Parent elements should control size of this component. Keep <svg>
+ * `width` property empty so that sizing can be handled by parent.
+ * 
+ * NOTE -- This file is ignored by Prettier.
+ */
 
 export default function BobWhatWouldYouSay() {
   return (
-    <div className={`${styles.BobWhatWouldYouSay}`}>
-      <div className={styles.foreground}>
-        {/*
-          TODO -- Could use some touch-up for mobile devices.
-        */}
-        {/* prettier-ignore */}
-        <div className={`absolute z-50 text-4xl font-semibold -mt-20 ml-[36vw] md:text-5xl md:ml-[36vw] lg:ml-[26vw] `}>
-          <div className={`${styles.foregroundtext}`}>{`"What would you say ...`}</div>
-          <div className={`${styles.foregroundtext} ml-[25%]`}>{`you do here?"`}</div>
-        </div>
-
-        <div className={styles.bobimagewrapper}>
-          <Image
-            src={BobTrianglePng}
-            priority={false}
-            quality={50}
-            alt=""
-            layout="intrinsic"
-          />
-        </div>
-      </div>
+    <div className={styles.BobWhatWouldYouSay}>
+      <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="100%" height="100%" viewBox="0 0 1344.651 524">
+        <style type="text/css">
+          {
+            `@font-face {
+              font-family: "Hoss Round",
+              src: url("https://use.typekit.net/zzs2pgb.css")
+            }`
+          }
+        </style>
+        <path id="Polygon_1" data-name="Polygon 1" d="M894.492,33.237l450.159,174.841L101.382,353.559Z" transform="translate(0 36.9)" fill="#b7afa3"/>
+        <text id="_What_would_you_say_..._you_do_here_" data-name="&quot;What would you say 
+          ... you do here?&quot;" transform="translate(553.663 107)" fill="#333" fontSize="72" fontFamily="HossRound-Bold, Hoss Round, sans-serif" fontWeight="700"><tspan x="0" y="67">&quot;What would you say </tspan><tspan x="0" y="153" xmlSpace="preserve">     ... you do here?&quot;</tspan></text>
+        <image id="guy_from_the_clip1" data-name="guy from the clip1" width="462" height="524" transform="translate(144.663)" xlinkHref={BobPng.src} />
+      </svg>
     </div>
   );
 }
