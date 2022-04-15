5e7ecd34b5fb7d46bfb6eff9ee1e0765861877bc
Lee <plbrannan95@gmail.com>
2022-04-15 10:23:32 -0400

    Add license and rights in footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -18,12 +18,28 @@ export default function Footer() {
         <div className="text-xl text-center">
           <h2 className="text-3xl">{`hi@bananabrann.dev`}</h2>
           <p>Certified Software Engineer</p>
-          <p>in New Mexico, United States of America</p>
+          <p>New Mexico, United States of America</p>
         </div>
       </div>
       <div className="text-sm mx-auto mt-5 text-center">
-        <p>{`"Banana Man" art assets CC 2.0, 2022`}</p>
-        <p>{`Website MIT license.`}</p>
+        <p>
+          {`"Banana Man" art by `}
+          <span className="hover:text-yellow-400 transition">
+            <Link href="https://www.artstation.com/gabemichaelis">
+              Gabe Michaelis
+            </Link>
+          </span>
+          {`. All rights reserved, 2022.`}
+        </p>
+        <p>
+          {`Website `}
+          <span className="hover:text-yellow-400 transition">
+            <Link href="https://github.com/bananabrann/bananabrann.dev/blob/main/LICENSE">
+              MIT license
+            </Link>
+          </span>
+          {`, 2022.`}
+        </p>
       </div>
     </div>
   );
