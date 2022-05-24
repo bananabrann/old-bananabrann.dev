01d39df51431493c611d3dfac9d2227aaad5688c
Lee <plbrannan95@gmail.com>
2022-05-24 09:34:37 -0400

    Posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,36 @@
 import BananamanStanding from "../components/BananamanStanding/BananamanStanding";
 import Layout from "../components/Layout/Layout";
 import DiscordBlackSvg from "../res/svg/discord-tiny-black.svg";
+import fs from "fs";
+import matter from "gray-matter";
+import { Post } from "../lib/interfaces/Post.interface";
+import Link from "next/link";
 
-export default function Home() {
+export async function getStaticProps() {
+  const files = fs.readdirSync("posts");
+
+  const posts: Post[] = files.map((fileName) => {
+    const slug = fileName.replace(".md", "");
+    const readFile = fs.readFileSync(`posts/${fileName}`, "utf-8");
+    const { data: frontmatter } = matter(readFile);
+
+    return {
+      slug,
+      frontmatter,
+    } as Post;
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+export default function Home({ posts }: { posts: Post[] }) {
   return (
     <Layout>
       <div className="flex items-center max-w-2xl mx-auto">
-
         <div className="max-w-[250px]">
           <BananamanStanding priority={true} />
         </div>
@@ -23,6 +47,19 @@ export default function Home() {
           </div>
         </div>
       </div>
+
+      {posts.map(({ slug, frontmatter }) => {
+        return (
+          <div key={slug}>
+            <Link href={`/posts/${slug}`}>
+              <div className="cursor-pointer">
+                <h2>{frontmatter.metaTitle}</h2>
+                <p>{frontmatter.metaDesc}</p>
+              </div>
+            </Link>
+          </div>
+        );
+      })}
     </Layout>
   );
 }
---
+++ posts/hello-world-blog-post.md
@@ -0,0 +1,13 @@
+---
+title: "Hello world!"
+metaTitle: "Hello world"
+metaDesc: "Hello world from my test blog post."
+socialImage: images/dog.jpg
+date: "2022-05-24"
+tags:
+  - code
+---
+
+# Hello world!
+
+wow, here is some content
